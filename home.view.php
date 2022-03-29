3b186863fe555b39008d4a487b238206e0979327
Octavio Ibarra Chambi <102199579+tavo16@users.noreply.github.com>
2022-03-28 23:20:13 -0400

    Create home.view.php

---
+++ home.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title>CUP</title>
+        <meta charset="UTF-8" />
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+        <style>
+            .home-page{
+            
+            background-image: url('logo.jpg');
+                background-position: center;
+                background-size: cover;
+            }
+        </style>
+	</head>
+    <body>
+        <div class=" home-page  rounded text-center w-100">
+            <!-- <img 
+                class='m-auto w-50' 
+                src="https://image.shutterstock.com/image-vector/faces-handsome-man-beautiful-woman-260nw-392964136.jpg" 
+                alt="imagen"> -->
+                
+        </div>
